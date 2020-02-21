a7fc2e0d1f6fcd068c36729727d91a9b674269e7
szkapec <szkapec999@o2.pl>
2020-02-21 09:03:39 +0100

    Updates

---
+++ precache-manifest.a8e99e62a167c3008effcbdc51f6de83.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db3acea8f6821b651b902f3778201b4b",
+    "url": "/React-router-articles/index.html"
+  },
+  {
+    "revision": "94cf20b8b5921cfd6dcb",
+    "url": "/React-router-articles/static/css/main.4b14b5f6.chunk.css"
+  },
+  {
+    "revision": "3da22f5b27e69a44c4c5",
+    "url": "/React-router-articles/static/js/2.e5019faa.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/React-router-articles/static/js/2.e5019faa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94cf20b8b5921cfd6dcb",
+    "url": "/React-router-articles/static/js/main.6a864de1.chunk.js"
+  },
+  {
+    "revision": "ba3b7fbded469d9f63d6",
+    "url": "/React-router-articles/static/js/runtime-main.7a2bc460.js"
+  },
+  {
+    "revision": "250c02868e5a837b98e5d95ebf85c18c",
+    "url": "/React-router-articles/static/media/danabramov.250c0286.jpg"
+  },
+  {
+    "revision": "3a72cbe0696789492f3395f24616b1dd",
+    "url": "/React-router-articles/static/media/kentcdodds.3a72cbe0.jpg"
+  },
+  {
+    "revision": "a87d0f352b87d3736e3487e389f37ff7",
+    "url": "/React-router-articles/static/media/logo.a87d0f35.svg"
+  },
+  {
+    "revision": "090afebd465406c24d1366b7d935c264",
+    "url": "/React-router-articles/static/media/michaeljackson.090afebd.jpg"
+  },
+  {
+    "revision": "5086cbe8d404204a12006334925c5ec6",
+    "url": "/React-router-articles/static/media/ryanflorence.5086cbe8.jpg"
+  }
+]);
